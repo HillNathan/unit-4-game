e6a8be139a360f6d05694e31fedc219d01686267
Nathan Hill <pjsstp@gmail.com>
2019-06-03 10:30:27 -0400

    figured out how I am going to clarify how to progress through the game: updates to game.js click functions to work with a game-stage variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,6 +27,12 @@ var player1 = {
     attack: 0,
 }
 
+var stage = "initial";
+// this var might require some explanation: 
+// I am using this to control what the click functions do. At different places in the game I want them to do different things. So 
+// with this variable I can change it when the user clicks through to a different phase of the game, and that then controls what 
+// the click functions do. 
+
 $(document).ready(function() {
 
     var lionHP = $("#lion-stats");
@@ -41,43 +47,51 @@ $(document).ready(function() {
     bearHP.text(theBear.hitPoints);
 
     $("#lion-card").on("click", function() {
-        $("#lion-card").remove();
-        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/lion.gif' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Lion</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
-        player1.hitPoints = theLion.hitPoints;
-        player1.attack = theLion.attack;
-        playerHP = $("#player1-stats");
-        playerHP.text(player1.hitPoints);
-        
+        if (stage == "initial") {
+            stage = "enemy-pick";
+            $("#lion-card").remove();
+            $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/lion.gif' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Lion</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+            player1.hitPoints = theLion.hitPoints;
+            player1.attack = theLion.attack;
+            playerHP = $("#player1-stats");
+            playerHP.text(player1.hitPoints);
+        }
     });
 
     $("#fox-card").on("click", function() {
-        // alert("you clicked the fox");
-        $("#fox-card").remove();
-        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/fox.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Fox</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
-        player1.hitPoints = theFox.hitPoints;
-        player1.attack = theFox.attack;
-        playerHP = $("#player1-stats");
-        playerHP.text(player1.hitPoints);        
+        if (stage == "initial") {
+            stage = "enemy-pick";
+            $("#fox-card").remove();
+            $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/fox.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Fox</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+            player1.hitPoints = theFox.hitPoints;
+            player1.attack = theFox.attack;
+            playerHP = $("#player1-stats");
+            playerHP.text(player1.hitPoints);      
+        } 
     });    
 
     $("#tiger-card").on("click", function() {
-        // alert("you clicked the tiger");
-        $("#tiger-card").remove();
-        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/tiger.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Tiger</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
-        player1.hitPoints = theTiger.hitPoints;
-        player1.attack = theTiger.attack;
-        playerHP = $("#player1-stats");
-        playerHP.text(player1.hitPoints);
+        if (stage == "initial") {
+            stage = "enemy-pick";
+            $("#tiger-card").remove();
+            $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/tiger.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Tiger</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+            player1.hitPoints = theTiger.hitPoints;
+            player1.attack = theTiger.attack;
+            playerHP = $("#player1-stats");
+            playerHP.text(player1.hitPoints);
+        }
     });
 
     $("#bear-card").on("click", function() {
-        // alert("you clicked the bear");
-        $("#bear-card").remove();
-        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/bear.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Bear</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
-        player1.hitPoints = theBear.hitPoints;
-        player1.attack = theBear.attack;
-        playerHP = $("#player1-stats");
-        playerHP.text(player1.hitPoints);
+        if (stage == "initial") {
+            stage = "enemy-pick";
+            $("#bear-card").remove();
+            $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/bear.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Bear</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+            player1.hitPoints = theBear.hitPoints;
+            player1.attack = theBear.attack;
+            playerHP = $("#player1-stats");
+            playerHP.text(player1.hitPoints);
+        }
     });
 
 
