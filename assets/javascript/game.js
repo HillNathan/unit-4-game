ccec30e185c2df23d98dc6249342d8084a99bf1e
Nathan Hill <pjsstp@gmail.com>
2019-06-03 07:26:40 -0400

    finished functions to move selecterd card to player area

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,37 +22,62 @@ var theFox = {
     counterAttack: 4,
 };
 
-
-
-
-
-
-
+var player1 = {
+    hitPoints: 0,
+    attack: 0,
+}
 
 $(document).ready(function() {
 
+    var lionHP = $("#lion-stats");
+    var tigerHP = $("#tiger-stats");
+    var foxHP = $("#fox-stats");
+    var bearHP = $("#bear-stats");
+    var playerHP = "";
 
-
-
-
-
-
+    lionHP.text(theLion.hitPoints);
+    tigerHP.text(theTiger.hitPoints);
+    foxHP.text(theFox.hitPoints);
+    bearHP.text(theBear.hitPoints);
 
     $("#lion-card").on("click", function() {
-        alert("you clicked the lion");
-        console.log("lion's attack is " + theLion.attack)
+        $("#lion-card").remove();
+        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/lion.gif' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Lion</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+        player1.hitPoints = theLion.hitPoints;
+        player1.attack = theLion.attack;
+        playerHP = $("#player1-stats");
+        playerHP.text(player1.hitPoints);
+        
     });
 
     $("#fox-card").on("click", function() {
-        alert("you clicked the fox");
+        // alert("you clicked the fox");
+        $("#fox-card").remove();
+        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/fox.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Fox</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+        player1.hitPoints = theFox.hitPoints;
+        player1.attack = theFox.attack;
+        playerHP = $("#player1-stats");
+        playerHP.text(player1.hitPoints);        
     });    
 
     $("#tiger-card").on("click", function() {
-        alert("you clicked the tiger");
+        // alert("you clicked the tiger");
+        $("#tiger-card").remove();
+        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/tiger.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Tiger</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+        player1.hitPoints = theTiger.hitPoints;
+        player1.attack = theTiger.attack;
+        playerHP = $("#player1-stats");
+        playerHP.text(player1.hitPoints);
     });
 
     $("#bear-card").on("click", function() {
-        alert("you clicked the bear");
+        // alert("you clicked the bear");
+        $("#bear-card").remove();
+        $("#user-battle").append("<div>").attr("class", "card m-2 float-left").attr("id", "player1").html("<img src='assets/images/bear.png' class='game-pic' alt='lion'><div class='card-body p-1'><h5>Bear</h5><P class='stats mb-0'><span id='player1-stats'></span> HP</P>" );
+        player1.hitPoints = theBear.hitPoints;
+        player1.attack = theBear.attack;
+        playerHP = $("#player1-stats");
+        playerHP.text(player1.hitPoints);
     });
 
 
